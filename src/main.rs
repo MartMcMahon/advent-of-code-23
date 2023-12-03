3a4bc10021e0ae0056ffc0bc4acb573651aada83
Mart <themcman@gmail.com>
2023-12-03 01:47:59 -0600

    day 3 star 1

--- src/main.rs
+++ src/main.rs
@@ -1,225 +1,250 @@
-use regex::Regex;
+use std::cmp::max;
+use std::collections::HashSet;
 use std::io::prelude::*;
+use std::io::{self};
 use std::{fs::File, io::BufReader};
 
-#[derive(Debug, PartialEq)]
-struct Game {
-    id: u32,
-    rounds: Vec<Round>,
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+struct PartNum {
+    coords: (usize, usize),
+    num: u32,
 }
-#[derive(Debug, PartialEq)]
-struct Round {
-    red: Option<u32>,
-    green: Option<u32>,
-    blue: Option<u32>,
+struct Matrix {
+    // data: [[char; 140]; 140],
+    data: Vec<Vec<char>>,
+    symbols_list: Vec<(usize, usize)>,
 }
-impl Round {
-    fn pow(&self) -> u32 {
-        let mut pow = 0;
-        let red = match self.red {
-            Some(r) => r,
-            None => 0,
-        };
-        let green = match self.green {
-            Some(g) => g,
-            None => 0,
-        };
-        let blue = match self.blue {
-            Some(b) => b,
-            None => 0,
-        };
-        red * green * blue
-    }
-}
-impl Game {
-    fn new() -> Self {
-        Self {
-            id: 0,
-            rounds: Vec::new(),
+impl Matrix {
+    fn from_lines(lines: &Vec<String>) -> Matrix {
+        let mut symbols_list = Vec::new();
+        let mut data = Vec::new();
+
+        for (y, line) in lines.iter().enumerate() {
+            let chars = line.chars().collect::<Vec<_>>();
+            data.push(chars.clone());
+            for (x, c) in chars.iter().enumerate() {
+                if *c != '.' && !c.is_digit(10) {
+                    println!("found {} at ({}, {})", *c, x, y);
+                    symbols_list.push((x, y));
+                }
+                //     matrix.data[y][x] = *c;
+            }
+        }
+
+        Matrix {
+            // data: [['.'; 140]; 140],
+            data,
+            symbols_list,
         }
     }
-    fn is_valid(&self) -> bool {
-        // {red: only 12 red cubes, 13 green cubes, and 14 blue cubes}
-        let max_red = 12;
-        let max_green = 13;
-        let max_blue = 14;
-
-        for round in &self.rounds {
-            if let Some(red) = round.red {
-                if red > max_red {
-                    return false;
-                }
-            }
-
-            if let Some(green) = round.green {
-                if green > max_green {
-                    return false;
-                }
-            }
-
-            if let Some(blue) = round.blue {
-                if blue > max_blue {
-                    return false;
-                }
-            }
+    fn get(&self, x: usize, y: usize) -> Option<char> {
+        if x >= self.data.len() || y >= self.data.len() {
+            return None;
+        } else {
+            Some(self.data[y][x])
         }
-        return true;
     }
-    fn get_min_cubes(&self) -> Round {
-        let mut min_red = 0;
-        let mut min_green = 0;
-        let mut min_blue = 0;
 
-        for round in &self.rounds {
-            if let Some(r) = round.red {
-                if r > min_red {
-                    min_red = r;
-                }
-            }
-            if let Some(g) = round.green {
-                if g > min_green {
-                    min_green = g;
-                }
-            }
-            if let Some(b) = round.blue {
-                if b > min_blue {
-                    min_blue = b;
-                }
+    fn get_number_at(&self, x: i32, y: i32) -> Option<PartNum> {
+        if x < 0 || y < 0 {
+            return None;
+        }
+        let x = x as usize;
+        let y = y as usize;
+        if self.get(x, y) == Some('.') {
+            return None;
+        }
+        if self.get(x, y).is_none() {
+            return None;
+        }
+
+        let mut n_chars: Vec<char> = Vec::new();
+        // add current digit
+        if let Some(c) = self.get(x, y) {
+            if c.is_digit(10) {
+                n_chars.push(c);
+            } else {
+                return None;
             }
         }
 
-        Round {
-            red: Some(min_red),
-            green: Some(min_green),
-            blue: Some(min_blue),
+        // go back and check prev
+        let mut start_x = x;
+        loop {
+            if start_x <= 0 {
+                break;
+            }
+            let c = self.get(start_x - 1, y);
+            if c == Some('.') || c == None {
+                break;
+            }
+            let c = c.unwrap();
+            if c.is_digit(10) == false {
+                break;
+            }
+
+            n_chars.splice(0..0, [c].iter().cloned());
+            start_x -= 1;
+            // println!("{:#?}", n_chars.iter().collect::<String>());
         }
+
+        let mut end_x = x;
+        loop {
+            if end_x >= 140 {
+                break;
+            }
+            let c = self.get(end_x + 1, y);
+            if c == None {
+                break;
+            }
+            let c = c.unwrap();
+            if c.is_digit(10) == false {
+                break;
+            }
+            n_chars.push(c);
+            end_x += 1;
+
+            // println!("{:#?}", n_chars.iter().collect::<String>());
+        }
+
+        let n = match n_chars.iter().collect::<String>().parse::<u32>() {
+            Ok(n) => n,
+            Err(e) => {
+                print!("{:#?} : {:#?}", e, n_chars.iter().collect::<String>());
+                0
+            }
+        };
+
+        Some(PartNum {
+            coords: (start_x, y),
+            num: n,
+        })
+    }
+
+    fn sum_parts(&self) -> u64 {
+        let mut read_coords: HashSet<(usize, usize)> = HashSet::new();
+        let mut part_sum: u64 = 0;
+        for symbol_coords in self.symbols_list.iter() {
+            let mut mods = Vec::new();
+            if symbol_coords.1 > 0 {
+                // check row above
+                mods.push((symbol_coords.0, symbol_coords.1 - 1));
+                if symbol_coords.0 > 0 {
+                    // check left
+                    mods.push((symbol_coords.0 - 1, symbol_coords.1 - 1));
+                }
+                if symbol_coords.0 < 139 {
+                    // check right
+                    mods.push((symbol_coords.0 + 1, symbol_coords.1 - 1));
+                }
+            }
+
+            if symbol_coords.1 < 139 {
+                // check row below
+                mods.push((symbol_coords.0, symbol_coords.1 + 1));
+                if symbol_coords.0 > 0 {
+                    mods.push((symbol_coords.0 - 1, symbol_coords.1 + 1));
+                }
+                if symbol_coords.0 < 139 {
+                    mods.push((symbol_coords.0 + 1, symbol_coords.1 + 1));
+                }
+            }
+
+            if symbol_coords.0 > 0 {
+                // check left
+                mods.push((symbol_coords.0 - 1, symbol_coords.1));
+            }
+            if symbol_coords.0 < 139 {
+                // check right
+                mods.push((symbol_coords.0 + 1, symbol_coords.1));
+            }
+
+            for m in mods {
+                if let Some(part) = self.get_number_at(m.0 as i32, m.1 as i32) {
+                    if !read_coords.contains(&(part.coords.0, part.coords.1)) {
+                        part_sum += part.num as u64;
+                        println!(
+                            "part #{} at ({}, {})",
+                            part.num, part.coords.0, part.coords.1
+                        );
+                    } else {
+                        println!(
+                            "part #{} at ({}, {}) already read. Not counting",
+                            part.num, part.coords.0, part.coords.1
+                        );
+                    }
+                    read_coords.insert((part.coords.0, part.coords.1));
+                }
+            }
+        }
+        part_sum
     }
 }
 
 fn main() {
-    println!("star 1: {}", star1().unwrap());
+    let input = read_input(3).unwrap();
+    println!("star 1: {}", star1(input.to_owned()).unwrap());
     println!("star 2: {}", star2().unwrap());
 }
 
-fn star1() -> anyhow::Result<u32> {
-    let f = File::open("input/2.txt")?;
+fn read_input(day: u8) -> io::Result<Vec<String>> {
+    let f = File::open(format!("input/{}.txt", day.to_string()))?;
     let f = BufReader::new(f);
-
-    let mut valid_games_sum = 0;
+    let mut lines = Vec::new();
     for line in f.lines() {
-        let line = line?;
-        let game = proc_line(line);
-        if game.is_valid() {
-            valid_games_sum += game.id
-        }
+        lines.push(line?);
     }
-
-    Ok(valid_games_sum)
+    Ok(lines)
 }
 
-fn proc_line(line: String) -> Game {
-    let split1 = line.split(":").collect::<Vec<&str>>();
-    let game_split = split1[0].split(" ").collect::<Vec<&str>>();
-    let id = game_split[1].parse::<u32>().unwrap();
+fn star1(input: Vec<String>) -> anyhow::Result<u64> {
+    let matrix = Matrix::from_lines(&input);
+    let part_sum = matrix.sum_parts();
 
-    let mut game = Game {
-        id,
-        rounds: Vec::new(),
-    };
-    let split2 = split1[1].split(";").collect::<Vec<&str>>();
-    for s in split2 {
-        let mut round = Round {
-            red: None,
-            green: None,
-            blue: None,
-        };
-        let re = Regex::new(r"(\d+) (\w+)").unwrap();
-        for (_full_cap, [num, color]) in re.captures_iter(s).map(|c| c.extract()) {
-            let n = num.parse::<u32>().unwrap();
-            match color {
-                "red" => round.red = Some(n),
-                "green" => round.green = Some(n),
-                "blue" => round.blue = Some(n),
-                _ => panic!("unknown color"),
-            }
-        }
-
-        game.rounds.push(round);
-    }
-    game
+    Ok(part_sum)
 }
 
 fn star2() -> anyhow::Result<i32> {
-    let f = File::open("input/2.txt")?;
-    let f = BufReader::new(f);
-
-    let mut game_pos_sum = 0;
-    for line in f.lines() {
-        let line = line?;
-        let game = proc_line(line);
-        let min_set: Round = game.get_min_cubes();
-        let pow = min_set.pow();
-        game_pos_sum += pow as i32;
-    }
-    Ok(game_pos_sum)
+    Ok(0)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const TEST_INPUT: &str = r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"#;
+    const INPUT: &str = r#"467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."#;
 
     #[test]
-    fn test_proc_line() {
-        assert_eq!(
-            proc_line("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green".to_string()),
-            Game {
-                id: 1,
-                rounds: vec![
-                    Round {
-                        red: Some(4),
-                        green: None,
-                        blue: Some(3),
-                    },
-                    Round {
-                        red: Some(1),
-                        green: Some(2),
-                        blue: Some(6),
-                    },
-                    Round {
-                        red: None,
-                        green: Some(2),
-                        blue: None,
-                    },
-                ],
-            }
-        );
+    fn test_star1() {
+        let input_lines: Vec<String> = INPUT.split("\n").map(|s| s.to_owned()).collect();
+        let matrix = Matrix::from_lines(&input_lines);
+
+        assert_eq!(matrix.data.len(), 10);
+        assert_eq!(matrix.data[0][0], '4');
+        assert_eq!(matrix.data[4][0], '6');
+
+        let pn = PartNum {
+            coords: (0, 0),
+            num: 467,
+        };
+        assert_eq!(matrix.get_number_at(0, 0), Some(pn.clone()));
+        assert_eq!(matrix.get_number_at(1, 0), Some(pn.clone()));
+        assert_eq!(matrix.get_number_at(2, 0), Some(pn.clone()));
+
+        assert_eq!(matrix.sum_parts(), 4361);
     }
 
     #[test]
     fn test_star2() {
-        let test_line_1 = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
-        let game = proc_line(test_line_1.to_string());
-        let min_set: Round = game.get_min_cubes();
-        let pow = min_set.pow();
-        assert_eq!(pow, 48);
-
-        let test_line_2 = "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue";
-        let game = proc_line(test_line_2.to_string());
-        let min_set: Round = game.get_min_cubes();
-        let pow = min_set.pow();
-        assert_eq!(pow, 12);
-
-        let test_line_3 =
-            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
-        let game = proc_line(test_line_3.to_string());
-        let min_set: Round = game.get_min_cubes();
-        let pow = min_set.pow();
-        assert_eq!(pow, 1560);
+        assert_eq!(super::star2().unwrap(), 0);
     }
 }
