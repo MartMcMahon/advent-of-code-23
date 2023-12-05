913f7f8147b65e3e9285fc71f6a45bb911e47883
Mart <themcman@gmail.com>
2023-12-04 22:54:07 -0600

    add read_lines to template

--- src/template.rs
+++ src/template.rs
@@ -2,18 +2,27 @@ use std::io::prelude::*;
 use std::io::{self};
 use std::{fs::File, io::BufReader};
 
-fn main() {
-    println!("star 1: {}", star1().unwrap());
-    println!("star 2: {}", star2().unwrap());
-}
-
-fn star1() -> anyhow::Result<i32> {
+fn read_lines() -> io::Result<io::Lines<BufReader<File>>> {
     let f = File::open("input/x.txt")?;
     let f = BufReader::new(f);
+    Ok(f.lines())
+}
 
+fn main() {
+    let mut lines = Vec::new();
+    for line in read_lines().unwrap() {
+        let line = line.unwrap();
+        lines.push(line);
+    }
+
+    println!("star 1: {}", star1(lines.clone()).unwrap());
+    println!("star 2: {}", star2(lines).unwrap());
+}
+
+fn star1(lines: Vec<String>) -> anyhow::Result<i32> {
     Ok(0)
 }
 
-fn star2() -> anyhow::Result<i32> {
+fn star2(lines: Vec<String>) -> anyhow::Result<i32> {
     Ok(0)
 }
