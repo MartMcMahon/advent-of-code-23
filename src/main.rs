46379cce178d5ee3761825f5647f67d774e58204
Mart <themcman@gmail.com>
2023-12-15 23:19:00 -0600

    start day 16

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-mod day15;
+mod day16;
 
 fn main() {
-    let buf = day15::read_input("input/15.txt");
-    let star1 = day15::star1(buf.clone());
-    let star2 = day15::star2(buf.clone());
+    let buf = day16::read_input("input/16.txt");
+    let star1 = day16::star1(buf.clone());
+    let star2 = day16::star2(buf.clone());
     println!("star 1: {:#?}", star1);
     println!("star 2: {:#?}", star2);
 }
